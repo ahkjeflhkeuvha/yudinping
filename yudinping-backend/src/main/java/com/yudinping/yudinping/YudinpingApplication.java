cb3391bc53c17bcff59440f829933cd5dc40c4f7
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-04-30 08:04:22 +0900

    setting

---
+++ yudinping-backend/src/main/java/com/yudinping/yudinping/YudinpingApplication.java
@@ -0,0 +1,13 @@
+package com.yudinping.yudinping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YudinpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YudinpingApplication.class, args);
+	}
+
+}
---
+++ yudinping-backend/src/test/java/com/yudinping/yudinping/YudinpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yudinping.yudinping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YudinpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
