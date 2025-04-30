913b1470353c70a2ef5afaa425728f9746f94f77
오지은 <w2317@e-mirim.hs.kr>
2025-04-30 13:44:20 +0900

    entity 설정

--- yudinping-backend/src/main/java/com/yudinping/yudinping/entity/ChatMessage.java
+++ yudinping-backend/src/main/java/com/yudinping/yudinping/entity/ChatMessage.java
@@ -1,5 +1,23 @@
 package com.yudinping.yudinping.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import lombok.Getter;   
+import lombok.Setter;
+
+@Entity
 public class ChatMessage {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String senderId;
+
+    @Column
+    private String receiverId;
+
+    @Column
+    private String message;
 }
--- yudinping-backend/src/main/java/com/yudinping/yudinping/entity/ChatRoomEntity.java
+++ yudinping-backend/src/main/java/com/yudinping/yudinping/entity/ChatRoomEntity.java
@@ -1,5 +1,25 @@
 package com.yudinping.yudinping.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import lombok.Getter;   
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class ChatRoomEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String chatroom_id;
+
+    @Column
+    private String chatroom_name;
+
+    @Column
+
 }
